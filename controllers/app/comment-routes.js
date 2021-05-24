c6848c5d9076219a3671d0272f726171180135aa
minamahmood83 <minamahmood83@gmail.com>
2021-05-24 16:53:32 -0700

    get comment

--- controllers/app/comment-routes.js
+++ controllers/app/comment-routes.js
@@ -2,3 +2,13 @@ const router = require("express").Router();
 const { User, Post, Comment } = require("../../models");
 //require
 const withAuth = require("../../utils/auth");
+
+//Get comments
+router.get("/", (req, res) => {
+    Comment.findAll()
+        .then((dbCommentData) => res.json(dbCommentData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
