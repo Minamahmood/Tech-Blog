af24b330141acb69f31fa299c6375ac13c8f58a9
minamahmood83 <minamahmood83@gmail.com>
2021-05-24 19:43:05 -0700

    connect session sequelize

--- server.js
+++ server.js
@@ -7,3 +7,10 @@ const sequelize = require("./config/connection");
 const helpers = require("./utils/helper");
 
 const exphbs = require("express-handlebars");
+
+const hbs = exphbs.create({
+    helpers,
+});
+const session = require("express-session");
+//connect-session-sequelize
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
