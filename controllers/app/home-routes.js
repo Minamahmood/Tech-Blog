742cf300559d5e057580af829b3307bb3110e38e
minamahmood83 <minamahmood83@gmail.com>
2021-05-24 18:45:28 -0700

    get router

--- controllers/app/home-routes.js
+++ controllers/app/home-routes.js
@@ -2,3 +2,27 @@ const router = require("express").Router();
 
 const sequelize = require("../config/connection");
 const { User, Post, Comment } = require("../models");
+//ROUTERget//
+router.get("/", (req, res) => {
+            Post.findAll({
+                    attributes: ["id", "title", "content", "created_at"],
+                    include: [{
+                            model: Comment,
+                            attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+                            include: {
+                                model: User,
+                                attributes: ["username"],
+                            },
+                        },
+                        {
+                            model: User,
+                            attributes: ["username"],
+                        },
+                    ],
+                })
+                .then((dbPostData) => {
+                        const posts = dbPostData.map((post) =>
+                            post.get({
+                                plain: true,
+                            })
+                        );
